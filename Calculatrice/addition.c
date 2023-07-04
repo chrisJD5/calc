8e1ad363954d9faaee48dc9731c5e4ba3b04b455
chrisJD5 <138492660+chrisJD5@users.noreply.github.com>
2023-07-04 12:05:52 +0200

    Delete Calculatrice directory

--- Calculatrice/addition.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include "operations.h"
-
-float addition(float a,float b)
-{
-
-    return a+b;
-
-}
--- Calculatrice/division.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include "operations.h"
-
-float division(float a,float b)
-{
-return (b!=0)?(float)a/b:0;
-}
